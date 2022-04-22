ee05ae3d34557b4bf24e510dfe7119c06c541783
Gersho <zennoune.karim@outlook.fr>
2022-04-22 23:30:06 +0200

    in case i break stuff

--- type_traits.hpp
+++ type_traits.hpp
@@ -6,13 +6,15 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 15:24:51 by kzennoun          #+#    #+#             */
-/*   Updated: 2022/04/21 15:57:42 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2022/04/22 23:20:42 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef TYPE_TRAITS_HPP
 # define TYPE_TRAITS_HPP
+
+# include <uchar.h>
 
 namespace ft
 {
@@ -31,113 +33,113 @@ namespace ft
 
 	template<class T> class is_integral
 	{
-		const bool value = false;
-		constexpr operator bool(){ return value; }
+		const static bool value = false;
+		// constexpr operator bool(){ return value; }
 	};
 
 	template<>
 	class is_integral<bool>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<char>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
-	template<>
-	class is_integral<char16_t>
-	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
-	};
+	// template<>
+	// class is_integral<char16_t>
+	// {
+	// 	const static bool value = true;
+	// 	// constexpr operator bool(){ return value; }		
+	// };
 
-	template<>
-	class is_integral<char32_t>
-	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
-	};
+	// template<>
+	// class is_integral<char32_t>
+	// {
+	// 	const static bool value = true;
+	// 	// constexpr operator bool(){ return value; }		
+	// };
 
 	template<>
 	class is_integral<wchar_t>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<signed char>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<short int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<long int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<long long int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<unsigned char>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<unsigned short int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<unsigned int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<unsigned long int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 	template<>
 	class is_integral<unsigned long long int>
 	{
-		const bool value = true;
-		constexpr operator bool(){ return value; }		
+		const static bool value = true;
+		// constexpr operator bool(){ return value; }		
 	};
 
 }
