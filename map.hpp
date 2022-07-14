f6c1bf37bf28be94dfdd6f2737291bcbe89f2c32
Gersho <zennoune.karim@outlook.fr>
2022-07-14 20:17:26 +0200

    whatever

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 15:21:52 by kzennoun          #+#    #+#             */
-/*   Updated: 2022/03/17 15:39:57 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2022/07/14 15:34:42 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,54 +16,20 @@
 
 
 
+namespace ft
+{
+
+	template < class Key,                                     // map::key_type
+			class T,                                       // map::mapped_type
+			class Compare = less<Key>,                     // map::key_compare
+			class Alloc = allocator<pair<const Key,T> >    // map::allocator_type
+			>
+	class map{}
+
+
+	
+}
+
+
+
 #endif
-
-/*
-
-# Map
-
-## Member functions
-
-- [x] `(constructor)`: Construct map
-- [x] `(destructor)`: Map destructor
-- [x] `operator=`: Copy container content
-
-#### Iterators:
-- [x] `begin`: Return iterator to beginning
-- [x] `end`: Return iterator to end
-- [x] `rbegin`: Return reverse iterator to reverse beginning
-- [x] `rend`: Return reverse iterator to reverse end
-
-#### Capacity:
-- [x] `empty`: Test whether container is empty
-- [x] `size`: Return container size
-- [x] `max_size`: Return maximum size
-
-#### Element access:
-- [x] `operator[]`: Access element
-
-#### Modifiers:
-- [x] `insert`: Insert elements
-- [x] `erase`: Erase elements
-- [x] `swap`: Swap content
-- [x] `clear`: Clear content
-
-#### Observers:
-- [x] `key_comp`: Return key comparison object
-- [x] `value_comp`: Return value comparison object
-
-#### Operations:
-- [x] `find`: Get iterator to element
-- [x] `count`: Count elements with a specific key
-- [x] `lower_bound`: Return iterator to lower bound
-- [x] `upper_bound`: Return iterator to upper bound
-- [x] `equal_range`: Get range of equal elements
-
-#### Allocator:
-- [x] `get_allocator`: Get allocator
-- [x] `swap`: Exchange contents of maps
-
-
-
-
-*/ 
